f2d8a5b6a541a03e9f7f3d19f1f1f2a39ac05791
Joachim Pileborg <arrow@pileborg.org>
2012-02-18 18:04:14 +0100

    Added basic source files

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/********************************************************************
+* File: main.cpp                               Part of Tutorial MUD *
+*                                                                   *
+* Created by Joachim Pileborg <tm@pileborg.org>                     *
+* This file is in the public domain.                                *
+********************************************************************/
+
+#include "tm.h"
+
+int main(int argc, char *argv[])
+{
+}
---
+++ src/tm.h
@@ -0,0 +1,10 @@
+#ifndef __TM_H__
+#define __TM_H__
+/********************************************************************
+* File: tm.h                                   Part of Tutorial MUD *
+*                                                                   *
+* Created by Joachim Pileborg <tm@pileborg.org>                     *
+* This file is in the public domain.                                *
+********************************************************************/
+
+#endif // __TM_H__
