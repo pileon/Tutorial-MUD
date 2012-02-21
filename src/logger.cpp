e238f44a88013c84b38bf5d099020013faa9bfca
Joachim Pileborg <arrow@pileborg.org>
2012-02-21 21:27:32 +0100

    Added files for logging

---
+++ src/logger.cpp
@@ -0,0 +1,10 @@
+/********************************************************************
+* File: logger.cpp                             Part of Tutorial MUD *
+*                                                                   *
+* Created by Joachim Pileborg <tm@pileborg.org>                     *
+* This file is in the public domain.                                *
+********************************************************************/
+
+#include "tm.h"
+#include "logger.h"
+
---
+++ src/logger.h
@@ -0,0 +1,14 @@
+#ifndef __LOGGER_H__
+#define __LOGGER_H__
+/********************************************************************
+* File: logger.h                               Part of Tutorial MUD *
+*                                                                   *
+* Created by Joachim Pileborg <tm@pileborg.org>                     *
+* This file is in the public domain.                                *
+********************************************************************/
+
+namespace logger
+{
+}
+
+#endif // __LOGGER_H__
