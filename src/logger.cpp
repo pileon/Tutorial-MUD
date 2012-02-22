9a6c55e8761990e74e669352fd0ef4fadf0174d8
Joachim Pileborg <arrow@pileborg.org>
2012-02-22 20:06:32 +0100

    Added helper class

--- src/logger.cpp
+++ src/logger.cpp
@@ -8,6 +8,41 @@
 #include "tm.h"
 #include "logger.h"
 
+#include <memory>
+#include <fstream>
+
+namespace
+{
+	class logger_helper
+	{
+	public:
+		logger_helper(const std::string &filename)
+			: m_file(filename), m_output(m_file)
+			{ }
+		logger_helper(std::ostream &file)
+			: m_output(file)
+			{ }
+		logger_helper()
+			: m_output(std::clog)
+			{ }
+
+		~logger_helper()
+			{
+				if (m_file.is_open())
+					m_file.close();
+			}
+
+		std::ostream &get()
+			{ return m_output; }
+
+	private:
+		std::ofstream m_file;    // File
+		std::ostream &m_output;  // Actual output stream used
+	};
+
+	std::unique_ptr<logger_helper> logger_helper_ptr;
+}
+
 void logger::init()
 {
 	// Use std::clog as output
