8f4ca8716fc5651c76d05cdc1fa617717fbd64a6
Joachim Pileborg <arrow@pileborg.org>
2012-03-04 11:23:05 +0100

    Added files for configuration

---
+++ src/config.cpp
@@ -0,0 +1,15 @@
+/********************************************************************
+* File: config.cpp                             Part of Tutorial MUD *
+*                                                                   *
+* Created by Joachim Pileborg <tm@pileborg.org>                     *
+* This file is in the public domain.                                *
+********************************************************************/
+
+#include "tm.h"
+#include "config.h"
+
+#include <unistd.h>
+
+void config::parse_argument(int argc, char *argv[])
+{
+}
---
+++ src/config.h
@@ -0,0 +1,15 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+/********************************************************************
+* File: config.h                               Part of Tutorial MUD *
+*                                                                   *
+* Created by Joachim Pileborg <tm@pileborg.org>                     *
+* This file is in the public domain.                                *
+********************************************************************/
+
+namespace config
+{
+	void parse_argument(int argc, char *argv[]);
+}
+
+#endif // __CONFIG_H__
